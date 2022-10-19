dcf038d561a9bbbcfdbf1109e8d07551faed4934
Venkata Jadda <venkat.jadda1991@gmail.com>
2022-10-18 23:52:50 -0500

    Initial commit

---
+++ src/test/java/com/example/amazonshoppingclientserviceforeurekha/AmazonShoppingClientServiceForEurekhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amazonshoppingclientserviceforeurekha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonShoppingClientServiceForEurekhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
